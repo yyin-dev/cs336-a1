c84f0b1dec940537fd9a643dd78e4fab05c16f9e
Yue Yin <yueyin.dev@gmail.com>
2025-07-09 08:08:26 -0400

    BPE adatper

--- tests/adapters.py
+++ tests/adapters.py
@@ -9,6 +9,7 @@ import numpy.typing as npt
 import torch
 from torch import Tensor
 
+from ..src.bpe import train_bpe
 
 
 def run_linear(
@@ -588,4 +589,4 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    train_bpe(input_path, vocab_size, special_tokens)
