750a71922309a2dc6fcaf97188982c0f0b9e5843
Yue Yin <yueyin.dev@gmail.com>
2025-07-10 10:16:09 -0400

    BPE without multiprocessing

--- src/bpe.py
+++ src/bpe.py
@@ -1,4 +1,26 @@
 import os
+import regex as re
+import collections
+from .pretokenization_example import find_chunk_boundaries
+
+
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+
+def get_subword_pair_freq(
+    pretoken_freq: dict[tuple, int],
+) -> dict[tuple[bytes, bytes], int]:
+    """
+    Args:
+        pretoken_freq (dict[tuple, int]). The key is a tuple of bytes
+    """
+    subword_pair_freq: dict[tuple[bytes, bytes], int] = collections.defaultdict(int)
+
+    for pretoken_subwords, freq in pretoken_freq.items():
+        for first, second in zip(pretoken_subwords, pretoken_subwords[1:]):
+            subword_pair_freq[(first, second)] += freq
+
+    return subword_pair_freq
 
 
 def train_bpe(
@@ -28,4 +50,65 @@ def train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    # TODO: Parallize pretokenization
+    escaped_special_tokens = list(map(re.escape, special_tokens))
+    special_tokens_regex = "|".join(escaped_special_tokens)
+    print(escaped_special_tokens)
+
+    pretoken_freq: dict[tuple, int] = collections.defaultdict(int)
+    with open(input_path, "r") as f:
+        # Split on special tokens, and pretokensize results separately.
+        # For example, [Doc 1]<ike [Doc 1]<|endoftext|>[Doc2], you should split
+        # on the special token <|endoftext|>, and pre-tokenize [Doc 1] and
+        # [Doc 2] separately, so that no merging can occur across the document
+        # boundary.
+        for part in re.split(special_tokens_regex, f.read()):
+            for pretoken in re.finditer(PAT, part):
+                pretoken = pretoken.group()
+                pretoken_bytes_singleton: bytes = pretoken.encode("utf-8")
+                individual_bytes: list[bytes] = [
+                    pretoken_bytes_singleton[i : i + 1]
+                    for i in range(len(pretoken_bytes_singleton))
+                ]
+
+                pretoken_freq[tuple(individual_bytes)] += 1
+
+    vocab: list[bytes] = list(map(int.to_bytes, list(range(0, 256))))
+    vocab.extend(list(map(lambda s: s.encode("utf-8"), special_tokens)))
+    print(f"Init vocab size (including special tokens): {len(vocab)}")
+
+    merges: list[tuple[bytes, bytes]] = []
+    while len(vocab) < vocab_size:
+        subword_pair_freq = get_subword_pair_freq(pretoken_freq)
+
+        # Stop if all pretokens have been merged
+        if len(subword_pair_freq) == 0:
+            break
+
+        def comp_freq_then_pair(pair):
+            pair_freq = subword_pair_freq[pair]
+            return (pair_freq, pair)
+
+        selected_pair = max(subword_pair_freq, key=comp_freq_then_pair)
+        new_vocab = selected_pair[0] + selected_pair[1]
+
+        merged_pretoken_freq: dict[tuple, int] = collections.defaultdict(int)
+        for pretoken_subwords, freq in pretoken_freq.items():
+            merged_pretoken_subwords: list[bytes] = [pretoken_subwords[0]]
+            for curr in pretoken_subwords[1:]:
+                prev = merged_pretoken_subwords[-1]
+                if (prev, curr) == selected_pair:
+                    merged_pretoken_subwords[-1] = new_vocab
+                else:
+                    merged_pretoken_subwords.append(curr)
+
+            merged_pretoken_freq[tuple(merged_pretoken_subwords)] = freq
+
+        print(f"Merge: {selected_pair} -> {new_vocab}")
+        pretoken_freq = merged_pretoken_freq
+
+        vocab.append(new_vocab)
+        merges.append(selected_pair)
+
+    vocab_dict = dict(enumerate(vocab))
+    return (vocab_dict, merges)
--- tests/adapters.py
+++ tests/adapters.py
@@ -589,4 +589,4 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    train_bpe(input_path, vocab_size, special_tokens)
+    return train_bpe(input_path, vocab_size, special_tokens)
